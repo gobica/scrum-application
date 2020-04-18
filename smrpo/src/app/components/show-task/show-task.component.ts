eb1196955fbb868dbc400e1f8453ee03236c623e
ZagoE <ez0057@student.uni-lj.si>
2020-04-18 18:41:34 +0200

    Dodan gumb za dodajnje nalog in dodan warning ce ni nalog za prikaz.

--- smrpo/src/app/components/show-task/show-task.component.ts
+++ smrpo/src/app/components/show-task/show-task.component.ts
@@ -31,9 +31,7 @@ export class ShowTaskComponent implements OnInit {
   allProjects = [];
   errors = [];
   allTasks = [{check: false, description: 'Prva naloga je lepa', size: 2.5, user: 'roberto'}, {check: false, description: 'Druga naloga je zanimiva', size: 3.0, user: ''}, {check: false, description: 'Tretja naloga je dolgočasna', size: 4.5, user: 'samantha'}];
-    //   description: string;
-    // size: number;
-    // user: string;
+  // allTasks = [];
 
 
   userNameTeamMember: string[] = [];
@@ -75,6 +73,10 @@ export class ShowTaskComponent implements OnInit {
     // });
     this.getAllUsers();
     this.getAllProjects();
+
+    if (this.allTasks.length < 1) {
+      this.alertService.warning('No tasks to show :)');
+    }
   }
 
   // private _filterTeamMember(value: string): string[] {
@@ -135,177 +137,177 @@ export class ShowTaskComponent implements OnInit {
     // return projects;
   }
 
-  initTasks() {
-    return this._fb.group({
-      taskDescription: ['',  Validators.required],
-      taskSize: ['',  Validators.required],
-      memberName: ['',  ],
-    });
-  }
-
-  addTask() {
-    const control = this.myForm.controls.tasks as FormArray;
-    control.push(this.initTasks());
-    let index = 0;
-    this.taskForm.value.task.tasks.forEach(m => {
-      let mN = this.taskForm.get('task').get('tasks').get([index]).get('memberName');
-      // console.log(mN);
-      // this.filteredOptionsTeamMember[index] = mN.valueChanges.pipe(
-      //   startWith(''),
-      //   map(value => this._filterTeamMember(value))
-      // );
-      index += 1;
-    });
-  }
-
-  removeTask(i: number) {
-    const control = this.myForm.controls.tasks as FormArray;
-    control.removeAt(i);
-  }
-
-  get f() {
-    return this.taskForm.controls;
-  }
-
-  isInDatabase(vsiUporabniki, uporabnik){
-    let jeVBazi = false;
-    vsiUporabniki.forEach(u => {
-      if(u.username === uporabnik) {
-        jeVBazi = true;
-      }
-    });
-    return jeVBazi;
-  }
-
-  onSubmit() {
-    this.submitted = true;
-    // reset alerts on submit
-    this.alertService.clear();
-    this.loading = true;
-
-    let dolzina = this.f.task.value.tasks.length;
-    const valueOfTasks = this.f.task.value.tasks;
-    // console.log(valueOfTasks);
-    let taskIndex = 1;
-    let correctForm = true;
-
-    valueOfTasks.forEach(task => {
-      // console.log(taskIndex);
-      // console.log(task);
-      if (correctForm === true) {
-        const description = task.taskDescription;
-
-        if (description !== undefined && description !== '' && description !== null) {
-          // console.log(description);
-          this.errors[taskIndex-1] = '';
-
-          const size = task.taskSize;
-          if (size !== undefined && size !== '' && size !== null) {
-            this.errors[taskIndex-1] = '';
-            const correctSize = size % 0.5;
-            if (correctSize === 0 && size > 0.4) {
-              // console.log(size);
-              this.errors[taskIndex-1] = '';
-              const user = task.memberName;
-              if(user !== undefined && user !== '' && user !== null) {
-                const jeVBazi= this.isInDatabase(this.allUsers, user);
-                if(jeVBazi === true) {
-                   // console.log(user);
-                  this.errors[taskIndex-1] = '';
-
-
-                } else {
-                  this.alertService.clear();
-                  this.alertService.error('The team member of the ' + taskIndex + '. task is not in the database.');
-                  this.errors[taskIndex-1] = 'Not in the database';
-                  let tmp = taskIndex;
-                  while(tmp < dolzina) {
-                    this.errors[tmp] = '';
-                    tmp += 1;
-                  }
-                  this.submitted = false;
-                  this.loading = false;
-                  correctForm = false;
-                }
-              }
-            } else {
-              this.alertService.clear();
-              this.alertService.error('The size of the ' + taskIndex + '. task doesn\'t have the right format. Size must be greater than 0.5 and decimal must be 0 or 5.');
-              this.errors[taskIndex-1] = 'Wrong format';
-              let tmp = taskIndex;
-              while(tmp < dolzina) {
-                this.errors[tmp] = '';
-                tmp += 1;
-              }
-              this.submitted = false;
-              this.loading = false;
-              correctForm = false;
-            }
-          } else {
-            this.alertService.clear();
-            this.alertService.error('The size of the ' + taskIndex + '. task is blank or has the wrong format. Please complete it correctly or delete the task.');
-            this.errors[taskIndex-1] = 'Empty size or wrong format';
-            let tmp = taskIndex;
-            while(tmp < dolzina) {
-              this.errors[tmp] = '';
-              tmp += 1;
-            }
-            this.submitted = false;
-            this.loading = false;
-            correctForm = false;
-          }
-        } else {
-          this.alertService.clear();
-          this.alertService.error('The description of the ' + taskIndex + '. task is blank. Please complete it or delete the task.');
-          this.errors[taskIndex-1] = 'Empty description';
-          let tmp = taskIndex;
-          while(tmp < dolzina) {
-            this.errors[tmp] = '';
-            tmp += 1;
-          }
-          this.submitted = false;
-          this.loading = false;
-          correctForm = false;
-        }
-         taskIndex += 1;
-      } else {
-        return false;
-      }
-
-    });
-
-    if (correctForm === true) {
-
-      if(dolzina >= 1) {
-        console.log(valueOfTasks);
-        this.alertService.success('New tasks created');
-        // console.log(dolzina);
-        let i = 1;
-
-        while (i < dolzina) {
-          // console.log(i);
-          this.removeTask(1);
-          i += 1;
-        }
-
-        // console.log(dolzina);
-
-        this.taskForm.get('task').get('tasks').get([0]).get('taskDescription').setValue('');
-        this.taskForm.get('task').get('tasks').get([0]).get('taskSize').setValue('');
-        this.taskForm.get('task').get('tasks').get([0]).get('memberName').setValue('');
-      } else {
-
-        // console.log("manjse enako 1");
-        this.alertService.warning('No task to add :(');
-      }
-
-
-    }
-
-
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-
-  }
+  // initTasks() {
+  //   return this._fb.group({
+  //     taskDescription: ['',  Validators.required],
+  //     taskSize: ['',  Validators.required],
+  //     memberName: ['',  ],
+  //   });
+  // }
+  //
+  // addTask() {
+  //   const control = this.myForm.controls.tasks as FormArray;
+  //   control.push(this.initTasks());
+  //   let index = 0;
+  //   this.taskForm.value.task.tasks.forEach(m => {
+  //     let mN = this.taskForm.get('task').get('tasks').get([index]).get('memberName');
+  //     // console.log(mN);
+  //     // this.filteredOptionsTeamMember[index] = mN.valueChanges.pipe(
+  //     //   startWith(''),
+  //     //   map(value => this._filterTeamMember(value))
+  //     // );
+  //     index += 1;
+  //   });
+  // }
+  //
+  // removeTask(i: number) {
+  //   const control = this.myForm.controls.tasks as FormArray;
+  //   control.removeAt(i);
+  // }
+  //
+  // get f() {
+  //   return this.taskForm.controls;
+  // }
+  //
+  // isInDatabase(vsiUporabniki, uporabnik){
+  //   let jeVBazi = false;
+  //   vsiUporabniki.forEach(u => {
+  //     if(u.username === uporabnik) {
+  //       jeVBazi = true;
+  //     }
+  //   });
+  //   return jeVBazi;
+  // }
+  //
+  // onSubmit() {
+  //   this.submitted = true;
+  //   // reset alerts on submit
+  //   this.alertService.clear();
+  //   this.loading = true;
+  //
+  //   let dolzina = this.f.task.value.tasks.length;
+  //   const valueOfTasks = this.f.task.value.tasks;
+  //   // console.log(valueOfTasks);
+  //   let taskIndex = 1;
+  //   let correctForm = true;
+  //
+  //   valueOfTasks.forEach(task => {
+  //     // console.log(taskIndex);
+  //     // console.log(task);
+  //     if (correctForm === true) {
+  //       const description = task.taskDescription;
+  //
+  //       if (description !== undefined && description !== '' && description !== null) {
+  //         // console.log(description);
+  //         this.errors[taskIndex-1] = '';
+  //
+  //         const size = task.taskSize;
+  //         if (size !== undefined && size !== '' && size !== null) {
+  //           this.errors[taskIndex-1] = '';
+  //           const correctSize = size % 0.5;
+  //           if (correctSize === 0 && size > 0.4) {
+  //             // console.log(size);
+  //             this.errors[taskIndex-1] = '';
+  //             const user = task.memberName;
+  //             if(user !== undefined && user !== '' && user !== null) {
+  //               const jeVBazi= this.isInDatabase(this.allUsers, user);
+  //               if(jeVBazi === true) {
+  //                  // console.log(user);
+  //                 this.errors[taskIndex-1] = '';
+  //
+  //
+  //               } else {
+  //                 this.alertService.clear();
+  //                 this.alertService.error('The team member of the ' + taskIndex + '. task is not in the database.');
+  //                 this.errors[taskIndex-1] = 'Not in the database';
+  //                 let tmp = taskIndex;
+  //                 while(tmp < dolzina) {
+  //                   this.errors[tmp] = '';
+  //                   tmp += 1;
+  //                 }
+  //                 this.submitted = false;
+  //                 this.loading = false;
+  //                 correctForm = false;
+  //               }
+  //             }
+  //           } else {
+  //             this.alertService.clear();
+  //             this.alertService.error('The size of the ' + taskIndex + '. task doesn\'t have the right format. Size must be greater than 0.5 and decimal must be 0 or 5.');
+  //             this.errors[taskIndex-1] = 'Wrong format';
+  //             let tmp = taskIndex;
+  //             while(tmp < dolzina) {
+  //               this.errors[tmp] = '';
+  //               tmp += 1;
+  //             }
+  //             this.submitted = false;
+  //             this.loading = false;
+  //             correctForm = false;
+  //           }
+  //         } else {
+  //           this.alertService.clear();
+  //           this.alertService.error('The size of the ' + taskIndex + '. task is blank or has the wrong format. Please complete it correctly or delete the task.');
+  //           this.errors[taskIndex-1] = 'Empty size or wrong format';
+  //           let tmp = taskIndex;
+  //           while(tmp < dolzina) {
+  //             this.errors[tmp] = '';
+  //             tmp += 1;
+  //           }
+  //           this.submitted = false;
+  //           this.loading = false;
+  //           correctForm = false;
+  //         }
+  //       } else {
+  //         this.alertService.clear();
+  //         this.alertService.error('The description of the ' + taskIndex + '. task is blank. Please complete it or delete the task.');
+  //         this.errors[taskIndex-1] = 'Empty description';
+  //         let tmp = taskIndex;
+  //         while(tmp < dolzina) {
+  //           this.errors[tmp] = '';
+  //           tmp += 1;
+  //         }
+  //         this.submitted = false;
+  //         this.loading = false;
+  //         correctForm = false;
+  //       }
+  //        taskIndex += 1;
+  //     } else {
+  //       return false;
+  //     }
+  //
+  //   });
+  //
+  //   if (correctForm === true) {
+  //
+  //     if(dolzina >= 1) {
+  //       console.log(valueOfTasks);
+  //       this.alertService.success('New tasks created');
+  //       // console.log(dolzina);
+  //       let i = 1;
+  //
+  //       while (i < dolzina) {
+  //         // console.log(i);
+  //         this.removeTask(1);
+  //         i += 1;
+  //       }
+  //
+  //       // console.log(dolzina);
+  //
+  //       this.taskForm.get('task').get('tasks').get([0]).get('taskDescription').setValue('');
+  //       this.taskForm.get('task').get('tasks').get([0]).get('taskSize').setValue('');
+  //       this.taskForm.get('task').get('tasks').get([0]).get('memberName').setValue('');
+  //     } else {
+  //
+  //       // console.log("manjse enako 1");
+  //       this.alertService.warning('No task to add :(');
+  //     }
+  //
+  //
+  //   }
+  //
+  //
+  //   document.body.scrollTop = document.documentElement.scrollTop = 0;
+  //
+  // }
 
   openDialog(i): void {
     const dialogRef = this.dialog.open(DialogCompletedTaskComponent, {
@@ -325,6 +327,11 @@ export class ShowTaskComponent implements OnInit {
       console.log('The dialog was closed: '+ result);
     });
   }
+
+  btnAddTask = function() {
+      this.router.navigateByUrl('/addTask');
+        // console.log(id);
+  };
 }
 
 
